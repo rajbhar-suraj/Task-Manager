c205a63a62971f988bccb8495b4be4f4e82f808b
rajbhar-suraj <your-email@example.com>
2025-06-02 23:50:59 +0530

    once again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react'
-import Input from './components/Input'
-import TaskList from './components/TaskList'
-import {TaskContextProvider} from './contexts/TaskContext';
+import React from 'react';
+import Input from './components/Input';
+import TaskList from './components/TaskList';
+import { TaskContextProvider } from './contexts/TaskContext';
 
 const App = () => {
-
   return (
     <TaskContextProvider>
-      <div className='bg-zinc-950 min-h-screen flex flex-col justify-center items-center'>
-        
-      <h1 className='text-2xl text-white font-extrabold'>Task Manager</h1>
+      <div className="bg-zinc-950 min-h-screen p-4 flex flex-col items-center">
+        <h1 className="text-xl md:text-3xl text-white font-extrabold mb-6 text-center">
+          Task Manager
+        </h1>
 
-      <div className='bg-zinc-800 h-90 w-170 rounded-md flex flex-col space-y-3 justify-center items-center'>
-        <Input />
+        {/* Input Container */}
+        <div className="bg-zinc-800 w-full max-w-xl rounded-md p-4 md:p-6 flex flex-col space-y-3 items-center shadow-md">
+          <Input />
+        </div>
+
+        {/* Task List Container */}
+        <div className="w-full max-w-xl mt-6">
+          <TaskList />
+        </div>
       </div>
-
-      <div className='rounded-md mt-4 flex '>
-        <TaskList />
-      </div>
-
-    </div>
     </TaskContextProvider>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
